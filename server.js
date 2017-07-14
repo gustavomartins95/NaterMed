e7bcf48f6136c2d186ab4c93b100fa9157d45f64
Gustavo Martins <gustavo_sh8@hotmail.com>
2017-07-14 17:36:36 -0300

    Pasta public e server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/*
+• body-parser: Todos os middlewares preencherão a propriedade req.body com o corpo analisado,
+ou um objeto vazio ({}) se não houver nenhum corpo para analisar.
+*/
+
+const bodyParser = require('body-parser'),
+      express = require('express'),
+      app = express()
+
+app.use(bodyParser.urlencoded({
+      extended: true
+}))
+app.use(bodyParser.json())
+app.use(express.static("public"))
+
+const port = 8080,
+      hostname = "localhost"
+app.listen(port, onStart())
+
+function onStart() {
+      console.log(`Server started at http://${hostname}:${port}`)
+}
