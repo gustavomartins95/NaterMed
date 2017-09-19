f5728ffe7e8a63472ffbad96ed46b517237efe4b
Gustavo Martins <gustavo_sh8@hotmail.com>
2017-09-19 10:09:08 -0300

    Nenhum resultado encontrado.

--- public/script/secretaria/retornarTableProfissional.js
+++ public/script/secretaria/retornarTableProfissional.js
@@ -13,16 +13,21 @@ jQuery(document).ready(function () {
 function criarTable(professionals) {
     // Limpa table
     $("#tbodyDadosProfissionais tr").remove()
-    for (index = 0; index < professionals.length; index++) {
-        var idprofissional = { id: professionals[index].idprofissional }
-        newTrItem = $("<tr>" +
-            "<td>" + professionals[index].nome_completo + "</td>" +
-            "<td>" + professionals[index].especialidade + "</td>" +
-            "<td>" + professionals[index].celular + "</td>" +
-            "<td>" + professionals[index].email + "</td>" +
-            "<td><a href='editartableprofissional?id=" + professionals[index].idprofissional + "' class='btn btn-primary' role='button'>Editar</a> " +
-            "<button type='button' class='btn btn-danger' onclick='excluirtableprofissional(" + JSON.stringify(idprofissional) + "); return false;'>Excluir</button></td>" +
-            "</tr>")
+    if (professionals.length) {
+        for (index = 0; index < professionals.length; index++) {
+            var idprofissional = { id: professionals[index].idprofissional }
+            newTrItem = $("<tr>" +
+                "<td>" + professionals[index].nome_completo + "</td>" +
+                "<td>" + professionals[index].especialidade + "</td>" +
+                "<td>" + professionals[index].celular + "</td>" +
+                "<td>" + professionals[index].email + "</td>" +
+                "<td><a href='editartableprofissional?id=" + professionals[index].idprofissional + "' class='btn btn-primary' role='button'>Editar</a> " +
+                "<button type='button' class='btn btn-danger' onclick='excluirtableprofissional(" + JSON.stringify(idprofissional) + "); return false;'>Excluir</button></td>" +
+                "</tr>")
+            appendTable(newTrItem)
+        }
+    } else {
+        newTrItem = $("<tr class='danger'><td colspan='5'>Nenhum resultado encontrado.</td></tr>")
         appendTable(newTrItem)
     }
 }
